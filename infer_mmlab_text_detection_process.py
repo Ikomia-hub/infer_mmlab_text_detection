3bdfea49621383c8a2c3dc32e2723fe83d7e2f91
Ludo <ludovic.barusseau@gmail.com>
2021-10-28 09:38:41 +0200

    Update requirements

--- infer_mmlab_text_detection_process.py
+++ infer_mmlab_text_detection_process.py
@@ -193,7 +193,7 @@ class InferMmlabTextDetectionFactory(dataprocess.CTaskFactory):
                                 "a config file (.py) and a model file (.pth). Both of these files are produced " \
                                 "by the train plugin."
         # relative path -> as displayed in Ikomia application process tree
-        self.info.path = "Plugins/Python"
+        self.info.path = "Plugins/Python/Text"
         self.info.version = "1.0.0"
         self.info.iconPath = "icons/mmlab.png"
         self.info.authors = "Kuang, Zhanghui and Sun, Hongbin and Li, Zhizhong and Yue, Xiaoyu and Lin," \
