11e40f027c1c3c9b17252d25bb6fdff0275c30e0
allankouidri <allan.kouidri@gmail.com>
2026-06-30 12:12:23 +0200

    Set Python version max = 3.11

--- infer_mmlab_text_detection_process.py
+++ infer_mmlab_text_detection_process.py
@@ -232,6 +232,7 @@ class InferMmlabTextDetectionFactory(dataprocess.CTaskFactory):
         self.info.path = "Plugins/Python/Text"
         self.info.version = "3.1.0"
         self.info.min_python_version = "3.10.0"
+        self.info.max_python_version = "3.11.0"
         self.info.min_ikomia_version = "0.16.0"
         self.info.icon_path = "icons/mmlab.png"
         self.info.authors = "Kuang, Zhanghui and Sun, Hongbin and Li, Zhizhong and Yue, Xiaoyu and Lin," \
